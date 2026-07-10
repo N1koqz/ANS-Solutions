8ed999847fd6d09d743c0b291eb557b381809a7d
Nicoooo <niko.adair@gmail.com>
2026-07-10 09:45:05 -0300

    Añadido archivos php y modificado index

---
+++ Sitio web/baseDeDatos.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "127.0.0.1";
+$username = "root";
+$password = "nico123";
+$dbname = "mgfut";
+$port = "3306"
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
+
+// Check connection
+if ($conn->connect_error) {
+  die("La conexion fallo: " . $conn->connect_error);
+}
+?>
